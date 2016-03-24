e555df5604fa7f57db76ea461da5039b08096bab
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2016-03-24 13:47:43 +0530

    Cleaned up redundant code

--- app/src/main/java/com/subhrajyoti/myappportfolio/MainActivity.java
+++ app/src/main/java/com/subhrajyoti/myappportfolio/MainActivity.java
@@ -13,70 +13,35 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button spotifyButton;
-    Button scoresButton;
-    Button libraryButton;
-    Button buildButton;
-    Button xyzButton;
-    Button capstoneButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        spotifyButton = (Button) findViewById(R.id.spotifyButton);
-        scoresButton = (Button) findViewById(R.id.scoresButton);
-        libraryButton = (Button) findViewById(R.id.libraryButton);
-        buildButton = (Button) findViewById(R.id.buildButton);
-        xyzButton = (Button) findViewById(R.id.xyzButton);
-        capstoneButton = (Button) findViewById(R.id.capstoneButton);
-
-        spotifyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    }
+    public void clicked(View view){
+        switch (view.getId()){
+            case R.id.spotifyButton:
                 Toast.makeText(getApplicationContext(),"This button will launch my Spotify Streamer app",Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        scoresButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.scoresButton:
                 Toast.makeText(getApplicationContext(),"This button will launch my Scores app",Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        libraryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.libraryButton:
                 Toast.makeText(getApplicationContext(),"This button will launch my Library app",Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        buildButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.buildButton:
                 Toast.makeText(getApplicationContext(),"This button will launch my Build it Bigger app",Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        xyzButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.xyzButton:
                 Toast.makeText(getApplicationContext(),"This button will launch my XYZ Reader app",Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        capstoneButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.capstoneButton:
                 Toast.makeText(getApplicationContext(),"This button will launch my Capstone app",Toast.LENGTH_SHORT).show();
+                break;
 
-            }
-        });
-
-
+        }
 
     }
 
